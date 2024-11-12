1e120f316abef039d1469557bdd4fae8a49c0fa1
HamzaElshennawy <h.elshennawy@nu.edu.eg>
2024-11-12 23:08:17 +0200

    Remove next-cloudinary dependency and related code from AnalyticsContent component

--- src/app/Dashboard/AnalyticsContent.tsx
+++ src/app/Dashboard/AnalyticsContent.tsx
@@ -1,18 +1,9 @@
 "use client";
 
-import { CldImage, CldImageProps } from "next-cloudinary";
-//import Image from "next/image";
-
 export default function AnalyticsContent() {
   return (
     <div>
       <h1>Analytics</h1>
-      <CldImage
-        alt="Cloudinary logo"
-        src="arrival3_yvf3gq"
-        width="300"
-        height="500"
-      />
     </div>
   );
 }
